11e9e8c0a5fdbaf3961452c05b307089ebc947b0
ykhalfao <ykhalfao@smail.uni-koeln.de>
2025-10-30 10:32:24 +0100

    Blatt2_p1

---
+++ src/p1/P1_main.java
@@ -0,0 +1,13 @@
+package p1;
+
+import java.util.Scanner;
+
+public class P1_main {
+public static void main(String []args) {	
+Scanner s = new Scanner(System.in);
+String zeile = new String();
+zeile = s.nextLine();
+System.out.println(zeile);
+s.close();
+}
+}
