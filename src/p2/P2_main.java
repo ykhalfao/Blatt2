947a2af954d7662ffad06d1e359051bd40c9711c
ykhalfao <ykhalfao@smail.uni-koeln.de>
2025-10-30 11:21:11 +0100

    Blatt_2-p2-p3

---
+++ src/p2/P2_main.java
@@ -0,0 +1,12 @@
+package p2;
+
+public class P2_main {
+	public static void main(String[] args) {
+		int i;
+		i=11;
+		boolean b;
+		b = false;
+		b = (i>10);
+		System.out.println(b);
+	}
+}
---
+++ src/p3/P3_main.java
@@ -0,0 +1,21 @@
+package p3;
+
+public class P3_main {
+public static void main(String[] args) {
+	int i;
+	i= 35;
+	int j;
+	j= 20;
+	int k;
+	k= 15;
+	if (i<j) {
+		System.out.print("A");
+	}
+	if (i<k) {
+		System.out.print ("B");
+	}
+	if (j<k) {
+		System.out.print ("C");
+	}
+}
+}
